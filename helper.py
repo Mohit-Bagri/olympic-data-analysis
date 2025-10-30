6cd7348cc37a08dc053ecb8523db2b14dbbec0a9
Mohit Bagri <mohitbagri@Mohits-MacBook-Air.local>
2025-10-30 14:35:17 +0530

    Removed a commented function from helper

--- helper.py
+++ helper.py
@@ -1,14 +1,4 @@
-# def medal_tally(df):
-#     medal_tally = df.drop_duplicates(subset=['Team', 'NOC', 'Games', 'Year', 'Season', 'City', 'Sport', 'Event', 'Medal'])
-#     medal_tally = medal_tally.groupby('region').sum()[['Gold', 'Silver', 'Bronze']].sort_values(by=['Gold'], ascending=False).reset_index()
-#     medal_tally["Total"] = medal_tally["Gold"] + medal_tally["Bronze"] + medal_tally["Silver"]
-#     medal_tally["Gold"] = medal_tally["Gold"].astype(int)
-#     medal_tally["Silver"]=medal_tally["Silver"].astype(int)
-#     medal_tally["Bronze"] =medal_tally["Bronze"].astype(int)
-#     medal_tally["Total"] = medal_tally["Total"].astype(int)
-#
-#     return medal_tally
-#
+
 
 def country_year_list(df):
     years = df["Year"].unique().tolist()
